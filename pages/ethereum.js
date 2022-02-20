a1911795714778d0ac5ae00963c1d329dee30504
Heri Kaniugu <hekan12@gmail.com>
2022-02-20 23:17:20 +0300

    Initial Commit

---
+++ pages/ethereum.js
@@ -0,0 +1,23 @@
+import { ethers, Contract } from "ethers";
+import Token from "./Token.json";
+
+const Blockchain = () => new Promise((resolve, reject) => {
+    window.addEventListener("load", async () => {
+        if (window.ethereum) {
+            await window.ethereum.enable();
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const signerAddress = await signer.getAddress();
+            const token = new Contract(
+                Token.address,
+                Token.abi,
+                signer
+            );
+
+            resolve({signerAddress, token});
+        }
+        resolve({signerAddress: undefined, token: undefined});
+    });
+});
+
+export default Blockchain;
