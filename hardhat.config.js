101ba1bf85f86890cc608e00d7e68df191e32c75
Heri Kaniugu <hekan12@gmail.com>
2022-02-21 19:09:09 +0300

    Updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,14 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
 
- require("@nomiclabs/hardhat-waffle");
+const secret = fs.readFileSync(".secret").toString();
 
- const INFURA_URL = "https://rinkeby.infura.io/v3/a1fbd7a559524a5abd4dd6fe6b248ca8";
- const PRIVATE_KEY = "a2f0585c209cc11d01fe1f2640a47b26a42fe494ac7ad181743109597caa088a";
- 
- module.exports = {
-   solidity: "0.8.0",
-   networks: {
-     rinkeby: {
-       url: INFURA_URL,
-       accounts: [`0x${PRIVATE_KEY}`]
-     }
-   }
- };
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/a1fbd7a559524a5abd4dd6fe6b248ca8",
+      accounts: [`0x${secret}`]
+    }
+  }
+};
